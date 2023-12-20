10719d543c0947b433a707a5b33b1b25186d211c
GuglioIsStupid <bradsbluetab@gmail.com>
2023-12-20 15:45:06 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gx Nitro Promo Generator
+
+Opera GX has a little flaw with its discord nitro promo,,,, its not that well done. So I made this little program that generates discord nitro with requests.
+
+I am not responsible for any damage done with this. This is purely for educational purposes.
---
+++ main.py
@@ -0,0 +1,37 @@
+import requests
+import json
+
+# Opera GX Nitro Promo
+
+amount = int(input("How many codes do you want to generate? "))
+print("Generating codes...")
+
+giftStartURL = "https://discord.com/billing/partner-promotions/1180231712274387115/"
+
+codes = []
+for i in range(amount):
+    r = requests.post("https://api.discord.gx.games/v1/direct-fulfillment", 
+                        headers={
+                            "authority": "api.discord.gx.games",
+                            "accept": "*/*",
+                            "accept-language": "en-US,en;q=0.9",
+                            "content-type": "application/json",
+                            "origin": "https://www.opera.com",
+                            "referer": "https://www.opera.com/",
+                            "sec-ch-ua": '"Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"',
+                            "sec-ch-ua-mobile": "?0",
+                            "sec-ch-ua-platform": '"Windows"',
+                            "sec-fetch-dest": "empty",
+                            "sec-fetch-mode": "cors",
+                            "sec-fetch-site": "cross-site",
+                            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0",
+                        },
+                        data=json.dumps({"partnerUserId": "this_can_be_anything"})
+                    ).json()
+    code = giftStartURL + r["token"]
+    codes.append(code)
+    
+# save codes to file
+with open("codes.txt", "w") as f:
+    for code in codes:
+        f.write(code + "\n")
