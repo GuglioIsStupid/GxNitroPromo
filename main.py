1a07af300057f76312fe7c9dac340f95983907b2
GuglioIsStupid <bradsbluetab@gmail.com>
2023-12-22 02:08:29 -0500

    Fix

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ print("Generating codes...")
 
 giftStartURL = "https://discord.com/billing/partner-promotions/1180231712274387115/"
 
+"""
+curl 'https://api.discord.gx.games/v1/direct-fulfillment' \
+  -H 'authority: api.discord.gx.games' \
+  -H 'accept: */*' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'content-type: application/json' \
+  -H 'origin: https://www.opera.com' \
+  -H 'referer: https://www.opera.com/' \
+  -H 'sec-ch-ua: "Opera GX";v="105", "Chromium";v="119", "Not?A_Brand";v="24"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: cross-site' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0' \
+  --data-raw '{"partnerUserId":"2bcedc970962a86fdb92c60ce684ee625c1fe9e97f36b5e0d0983f80d57a1fb6"}' \
+  --compressed
+"""
+
 codes = []
 for i in range(amount):
     try:
@@ -28,7 +47,7 @@ for i in range(amount):
                             "sec-fetch-site": "cross-site",
                             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 OPR/105.0.0.0",
                         },
-                        data=json.dumps({"partnerUserId": "this_can_be_anything_num_{}".format(str(random.randint(1, 10000000)))})
+                        data=json.dumps({"partnerUserId": "2bcedc970962a86fdb92c60ce684ee625c1fe9e97f36b5e0d0983f80d57a1fb6"})
                     ).json()
         
         code = giftStartURL + r["token"]
